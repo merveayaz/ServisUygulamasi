1b2622e6244a7bfd3317a7f7e604ba1cc5910a99
Merve Ayaz <ayaz.merve1998@gmail.com>
2020-07-28 12:56:47 +0300

    Backend baslangic

---
+++ src/main/java/com/user/crudApp/CrudAppApplication.java
@@ -0,0 +1,15 @@
+package com.user.crudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppApplication.class, args);
+		System.out.println("Welcome to the spring web");
+		System.out.println("Bu deneme icin");
+	}
+
+}
