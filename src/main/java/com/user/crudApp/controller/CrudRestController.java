cd092014d892c81dc4e03f140cc9e7780e80a2ef
Merve Ayaz <ayaz.merve1998@gmail.com>
2020-07-31 00:21:05 +0300

    with get

--- src/main/java/com/user/crudApp/controller/CrudRestController.java
+++ src/main/java/com/user/crudApp/controller/CrudRestController.java
@@ -1,8 +1,28 @@
 package com.user.crudApp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.user.crudApp.model.User;
+import com.user.crudApp.services.CrudService;
 
 @RestController
 public class CrudRestController {
-
+	@Autowired
+	private CrudService service;
+	
+	
+	@GetMapping("/getuserlist")
+	public List<User> fetchUserList() {
+		List<User> users = new ArrayList<User>();
+		//Database'den veri getirme 
+		users = service.fetchUserList();
+		return users;
+	}
 }
--- src/main/java/com/user/crudApp/services/CrudService.java
+++ src/main/java/com/user/crudApp/services/CrudService.java
@@ -1,8 +1,19 @@
 package com.user.crudApp.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.user.crudApp.model.User;
+import com.user.crudApp.repo.CrudRepo;
 
 @Service
 public class CrudService {
-
+	@Autowired
+	private CrudRepo repo;
+	
+	public  List<User> fetchUserList() {
+		return repo.findAll();
+	}
 }
